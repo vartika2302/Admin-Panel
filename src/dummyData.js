970ae8f80902e809ce493c160b8ca2e6da82e077
Vartika <guptavartika624@gmail.com>
2022-01-23 20:23:01 +0530

    Add chart

---
+++ src/dummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+  {
+    name: "Jan",
+    "Active Users": 4000,
+  },
+  {
+    name: "Feb",
+    "Active Users": 3000,
+  },
+  {
+    name: "Mar",
+    "Active Users": 5000,
+  },
+  {
+    name: "April",
+    "Active Users": 2000,
+  },
+  {
+    name: "May",
+    "Active Users": 4000,
+  },
+  {
+    name: "June",
+    "Active Users": 3000,
+  },
+  {
+    name: "July",
+    "Active Users": 5000,
+  },
+  {
+    name: "Aug",
+    "Active Users": 6000,
+  },
+  {
+    name: "Sept",
+    "Active Users": 4000,
+  },
+  {
+    name: "Oct",
+    "Active Users": 3000,
+  },
+  {
+    name: "Nov",
+    "Active Users": 5000,
+  },
+  {
+    name: "Dec",
+    "Active Users": 4000,
+  },
+];
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,14 @@
+import Chart from '../../components/chart/Chart';
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
 import './home.scss';
+import { userData } from '../../dummyData';
 
 
 export default function Home() {
   return (
       <div className='home'>
           <FeaturedInfo/>
+          <Chart data={userData} title="User Analytics" grid dataKey="Active Users"/>
       </div>
   );
 }
